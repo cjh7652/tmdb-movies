62e8b357b085e8bf80f68d3928a94b1df8c20034
jeehyun <cjh7652@hanmail.net>
2024-12-17 15:36:44 +0900

    actors

--- src/pages/SearchDetail.jsx
+++ src/pages/SearchDetail.jsx
@@ -2,9 +2,11 @@ import React,{useState, useEffect}from 'react';
 //useEffect 생명주기를 사용할수 있는 훅
 import { useParams } from 'react-router-dom'; //url 주소의 파라미터값 추출
 import axios from 'axios';
+import './SearchDetail.scss';
 
 
 const SearchDetail = () => {
+  const imgPath='http://image.tmdb.org/t/p/original';// tmdb의 이미지경로
   const {movieId} = useParams(); //각각의 아이디값
   const [detailInfo, setDetailInfo]=useState({}); //선택된 영화의 상세정보
   const [actors, setActors]=useState([]); //선택된 영화의 출연배우정보 저장
@@ -12,7 +14,8 @@ const SearchDetail = () => {
   const fetchMovieDetails = async () => {
     try{
       const response= await axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=546c72b99cf64514c2c03c7ef473011b&language=ko`)
-      setActors(response.data.cast)
+      setActors(response.data.cast);
+      console.log(response.data.cast)
     } catch(error){
       console.error('Error fetching movie details', error)
     }
@@ -20,6 +23,7 @@ const SearchDetail = () => {
     try{
       const response= await axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=546c72b99cf64514c2c03c7ef473011b&language=ko`)
       setDetailInfo(response.data)
+      console.log(response.data)
     } catch(error){
       console.error('Error fetching movie details', error)
     }
@@ -33,13 +37,13 @@ const SearchDetail = () => {
     <div className='searchDetail'>
       <div className="movieInfo">
         <div className="search-backImg">
-          <img src="" alt="" />
+          <img src={`${imgPath}${detailInfo.backdrop_path}`} alt={detailInfo.title} />
         </div>
         <div className="search-info">
-          <p className="search-title"></p>
-          <p className="search-originTit"></p>
-          <p className="search-desc"></p>
-          <p className="search-release"></p>
+          <p className="search-title">{detailInfo.title}</p>
+          <p className="search-originTit">{detailInfo.original_title}</p>
+          <p className="search-desc">{detailInfo.overview}</p>
+          <p className="search-release">{detailInfo.release_date}</p>
         </div>
       </div>
       <hr />
@@ -48,10 +52,17 @@ const SearchDetail = () => {
           <h3>Actors</h3>
           <ul className="actor-list">
             {
-              as.map((a)=> (
-                <div>
-
-                </div>
+              actors.slice(0,12).map((actor)=> (
+                <li key={actor.id} className='actor-item'>
+                    <div className="search-img">
+                      <img src={`${imgPath}${actor.profile_path}`} alt={actor.name} />
+                    </div>
+                    <div className="search-info">
+                      <p className="actor-name">{actor.name}</p>
+                   
+                      <p className="actor-character">{actor.character}</p>
+                    </div>
+                </li>
               ))
             }
           </ul>
